7903dca5ee76781d88e7737ba41d59701a1056a0
Максим Д. <115506198+MaksimRider@users.noreply.github.com>
2026-02-03 17:32:08 +0200

    Add files via upload

---
+++ PR2SLP2/PR2SLP2/Program.fs
@@ -0,0 +1,21 @@
+﻿let rec arithmeticProgression a0 d n =
+    if n <= 0 then []
+    else a0 :: arithmeticProgression (a0 + d) d (n - 1)
+
+let sumOfList list =
+    List.sum list
+
+
+[<EntryPoint>]
+let main _ =
+    let a0 = 3
+    let d = 2
+    let n = 8
+
+    let progression = arithmeticProgression a0 d n
+    let sum = sumOfList progression
+
+    printfn "Арифметична прогресiя: %A" progression
+    printfn "Сума елементiв: %d" sum
+
+    0
