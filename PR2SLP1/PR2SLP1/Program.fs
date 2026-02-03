482c2c23d1694496eaaee8c1246d92b21774aea8
Максим Д. <115506198+MaksimRider@users.noreply.github.com>
2026-02-03 17:29:39 +0200

    Add files via upload

---
+++ PR2SLP1/PR2SLP1/Program.fs
@@ -0,0 +1,16 @@
+﻿let rec factorial n =
+    if n = 0 || n = 1 then 1
+    else n * factorial (n - 1)
+
+
+[<EntryPoint>]
+let main _ =
+    let tests = [0; 1; 5; 7; 10]
+
+    tests
+    |> List.map (fun x -> x, factorial x)
+    |> List.iter (fun (x, result) ->
+        printfn "factorial %d = %d" x result
+    )
+
+    0
